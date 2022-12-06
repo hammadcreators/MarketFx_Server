7ca0b612fae84274476e3a1e216fe966e7adeb6c
hammadcreators <hammadali648@gmail.com>
2022-12-06 11:39:15 +0500

    Base code

---
+++ app/models/CustomerSupport.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const CustomerSupportSchema = new mongoose.Schema({
+  // name price, description duration
+
+  name: {
+    type: String,
+    required: [true, "Name is a required field"],
+  },
+
+  email: {
+    type: String,
+    required: [true, "Email is a required field"],
+    unique: true,
+    match:
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  },
+
+  password: {
+    type: String,
+    required: [true, "Password is required field"],
+  },
+
+});
+
+const Package = mongoose.model("CustomerSupport", CustomerSupportSchema);
+module.exports = CustomerSupport;
--- app/models/Package.js
+++ app/models/Package.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const packageSchema = new mongoose.Schema({
+    
     // name price, description duration 
 
     name: {
